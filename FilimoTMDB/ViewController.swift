bd27885c46c9b9137f46793c7c7e59bb6ea0be46
Amir Ehsanijam <amir.ehsani.jam@gmail.com>
2022-10-24 11:49:39 +0330

    Initial Commit

---
+++ FilimoTMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilimoTMDB
+//
+//  Created by Amir on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
